113fcc15dff6c797351daf9225c2127eba1fda55
Number-4 <82287793+Number-44@users.noreply.github.com>
2024-12-06 01:19:41 +0330

    Update python_flag.py

--- python_flag.py
+++ python_flag.py
@@ -1,27 +1,18 @@
 python = "Python"
 x = 0
-for i in python:
-    x += 1
-    print(python[0:x])
+for _ in range(2):
+    for _ in python:
+        x += 1
+        print(python[0:x])
 
-for i in python:
-    x -= 1
-    print(python[0:x])
+    for _ in python:
+        x -= 1
+        print(python[0:x])
 
-python = "Python"
-x = 0
-for i in python:
-    x += 1
-    print(python[0:x])
-
-for i in python:
-    x -= 1
-    print(python[0:x])
-
-z = input("enter a number : ")
+z = input("enter a number \" recommonded from 3 to 10 \" and press \" enter \" : ")
 z = int(z)
 
 for item in range(z,0,-1):
     print(item * " * ")
-for item in range(0,z):
+for item in range(0,z+1):
     print(item * " * ")
