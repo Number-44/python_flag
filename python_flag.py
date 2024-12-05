5048f53808e28b4b07d81201d952e800c8468082
Number-4 <82287793+Number-44@users.noreply.github.com>
2024-12-06 01:39:48 +0330

    Update python_flag.py

--- python_flag.py
+++ python_flag.py
@@ -1,6 +1,9 @@
+import time
+
 python = "Python"
-x = 0
+
 for _ in range(2):
+    x = 0
     for _ in python:
         x += 1
         print(python[0:x])
@@ -8,11 +11,18 @@ for _ in range(2):
     for _ in python:
         x -= 1
         print(python[0:x])
+while True:
+    z = input("enter a number \" recommonded from 3 to 10 \" and press \" enter \" : ")
+    if z.isdigit() and 3 <= int(z) <= 10 :
+        z = int(z)
+        break
 
-z = input("enter a number \" recommonded from 3 to 10 \" and press \" enter \" : ")
-z = int(z)
-
+    print(f"{z} is not valid : Please enter a number in the given range !")
 for item in range(z,0,-1):
     print(item * " * ")
 for item in range(0,z+1):
     print(item * " * ")
+
+
+
+print(f"the process time is :{time.process_time()} seconds " )
