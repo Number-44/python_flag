16a98ce77b7209f5bb88a0c27392a87f2f8d6881
Number-4 <82287793+Number-44@users.noreply.github.com>
2024-12-06 02:21:15 +0330

    Update python_flag.py

--- python_flag.py
+++ python_flag.py
@@ -4,20 +4,22 @@ python = "Python"
 
 for _ in range(2):
     x = 0
-    for _ in python:
+    for item in python:
         x += 1
         print(python[0:x])
 
-    for _ in python:
+    for item in python:
         x -= 1
         print(python[0:x])
+
 while True:
-    z = input("enter a number \" recommonded from 3 to 10 \" and press \" enter \" : ")
-    if z.isdigit() and 3 <= int(z) <= 10 :
+    z = input("Enter a number (recommended from 3 to 10) and press 'Enter': ")
+    if z.isdigit() and 3 <= int(z) <= 10:
         z = int(z)
         break
+    else:
+        print(f"{z} is not valid. Please enter a number in the given range!")
 
-    print(f"{z} is not valid : Please enter a number in the given range !")
 for item in range(z,0,-1):
     print(item * " * ")
 for item in range(0,z+1):
